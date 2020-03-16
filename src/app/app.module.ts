a819fa987d506f5c6b179e778c65bfb91574f2e1
Hector Sanchez <20186183@itla.edu.do>
2020-03-16 04:19:29 -0400

    Componente y elementos para el Historial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculadoraComponent } from './Componentes/calculadora/calculadora.component';
 import { NavegationComponent } from './Componentes/navegation/navegation.component';
+import { HistorialComponent } from './Componentes/historial/historial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculadoraComponent,
-    NavegationComponent
+    NavegationComponent,
+    HistorialComponent
   ],
   imports: [
     BrowserModule,
