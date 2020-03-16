63f667ae3df683595a381fa132053ab8aba8f7dc
Hector Sanchez <20186183@itla.edu.do>
2020-03-16 03:17:58 -0400

    Componentes agregados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './Componentes/calculadora/calculadora.component';
+import { NavegationComponent } from './Componentes/navegation/navegation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraComponent,
+    NavegationComponent
   ],
   imports: [
     BrowserModule,
